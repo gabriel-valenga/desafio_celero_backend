df7559e209915d879b355f5ccb5bace209f9de23
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-11-23 19:26:58 -0300

    Feita instalação e configuração do Django e Django Rest Framework; Criado GitIgnore; Criados models baseados nos arquivos csv; Configurado admin

---
+++ olympichistory/competition_history_app/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import (
+    Noc,
+    Atleta,
+    Time,
+    Olimpiada,
+    Competicao,
+    CompeticaoAtleta
+)
+
+
+@admin.register(Noc)
+class NocAdmin(admin.ModelAdmin):
+    list_display = ('sigla', 'regiao', 'criacao_registro', 'atualizacao_registro')
+
+
+@admin.register(Atleta)
+class AtletaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'criacao_registro', 'atualizacao_registro')
+
+
+@admin.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'noc', 'criacao_registro', 'atualizacao_registro')
+
+
+@admin.register(Olimpiada)
+class OlimpiadaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ano', 'criacao_registro', 'atualizacao_registro')
+
+
+@admin.register(Competicao)
+class CompeticaoAdmin(admin.ModelAdmin):
+    list_display = ('olimpiada', 'esporte', 'modalidade', 'criacao_registro', 'atualizacao_registro')
+
+
+@admin.register(CompeticaoAtleta)
+class CompeticaoAtletaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'competicao', 'atleta', 'criacao_registro', 'atualizacao_registro')
---
+++ olympichistory/competition_history_app/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+
+SEXO = (
+    ('F', 'Feminino'),
+    ('M', 'Masculino'),
+    ('O', 'Outro')
+)
+
+ESTACOES_ANO_OLIMPICAS = (
+    ('Summer', 'Verão'),
+    ('Winter', 'Inverno')
+)
+MEDALHA = (
+    ('Golden', 'Ouro'),
+    ('Silver', 'Prata'),
+    ('Bronze', 'Bronze')
+)
+
+
+class Base(models.Model):
+    criacao_registro = models.DateTimeField(auto_now_add=True),
+    atualizacao_registro = models.DateTimeField(auto_now=True),
+
+    class Meta:
+        abstract = True
+
+
+class Noc(Base):
+    sigla = models.CharField(max_length=3, unique=True, blank=False)
+    regiao = models.CharField(max_length=50, blank=False)
+    observacoes = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name = 'Noc'
+        verbose_name_plural = 'Nocs'
+
+
+class Atleta(Base):
+    nome = models.CharField(max_length=100, blank=False)
+    sexo = models.CharField(max_length=1, choices=SEXO, blank=False)
+
+    class Meta:
+        verbose_name = 'Atleta'
+        verbose_name_plural = 'Atletas'
+
+
+class Time(Base):
+    nome = models.CharField(max_length=70, unique=True, blank=False)
+    noc = models.ForeignKey(Noc, max_length=3, blank=False, on_delete=models.RESTRICT)
+
+    class Meta:
+        verbose_name = 'Time'
+        verbose_name_plural = 'Times'
+
+
+class Olimpiada(Base):
+    nome = models.CharField(max_length=100, unique=True, blank=False)
+    ano = models.IntegerField(null=False)
+    estacao = models.CharField(max_length=6, choices=ESTACOES_ANO_OLIMPICAS, blank=False)
+    cidade = models.CharField(max_length=70, blank=False)
+
+    class Meta:
+        verbose_name = 'Olimpíada'
+        verbose_name_plural = 'Olimpíadas'
+
+
+class Competicao(Base):
+    olimpiada = models.ForeignKey(Olimpiada, null=False, on_delete=models.RESTRICT)
+    esporte = models.CharField(max_length=40, blank=False)
+    modalidade = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        models.UniqueConstraint(fields=['olimpiada', 'esporte', 'modalidade'], name='unique_competicao')
+        verbose_name = 'Competição'
+        verbose_name_plural = 'Competições'
+
+    def __str__(self):
+        return f'{self.olimpiada} - {self.esporte}, {self.modalidade}'
+
+
+class CompeticaoAtleta(Base):
+    competicao = models.ForeignKey(Competicao, null=False, on_delete=models.RESTRICT)
+    atleta = models.ForeignKey(Atleta, null=False, on_delete=models.RESTRICT)
+    idade_atleta = models.IntegerField(null=True)
+    altura_atleta = models.DecimalField(null=True, decimal_places=2, max_digits=3)
+    peso_atleta = models.DecimalField(null=True, decimal_places=2, max_digits=5)
+    medalha = models.CharField(max_length=6, choices=MEDALHA, blank=True)
+
